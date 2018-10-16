d396dbbd0ecfd23e0749e55d268c331c28c3ff74
rsbear <broderross@gmail.com>
2018-10-16 11:17:24 -0500

    maybe

--- functionsStripe/fetchProducts.js
+++ functionsStripe/fetchProducts.js
@@ -1,31 +1,19 @@
 import stripePackage from "stripe";
 
 export async function main(event, context, callback) {
+  const { products } = JSON.parse(event.body);
+  const description = "Revive Archives charge";
+
+  // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
-  return stripe.products.list(
-      {limit: 10}).then((products) => {
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({
-        data: products.data
-      }),
-    };
-    callback(null, response);
-  }).catch((err) => { // Error response
-    console.log(err);
-    const response = {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({
-        error: err.message,
-      }),
-    };
-    callback(null, response);
-  });
+
+  try {
+    return stripe.products.list({ 
+      limit: 20,
+      products
+    }) 
+  } catch (e) {
+    callback(null, failure({ message: e.message }));
+  }
 }
 
